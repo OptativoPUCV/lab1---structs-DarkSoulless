a8aa3360c508abdc514fe7cca5e0b8cabd54456f
Replit user <>
2023-03-18 21:49:02 +0000

    new try

--- exercises.c
+++ exercises.c
@@ -28,12 +28,8 @@ void arrayMaxMin(int *a, int n, int *max, int *min){
   *max = a[0];
   *min = a[0];
   for (int i = 0; i < n; i++){
-    if (a[i] > *max){ 
-      *max = a[i];
-    }
-    else(a[i] < *min){
-      *min = a[i];
-    }
+    if (a[i] > *max) *max = a[i];
+    else(a[i] < *min) *min = a[i];
   }
 }
 
